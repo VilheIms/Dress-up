957949b0df6689964468b0f23e278640ab068756
VilheIms <boysepticeye@gmail.com>
2026-02-24 18:12:42 +0200

    Tagad var mainīt character size un width, tomēr jāizdomā tā lai tie nerestartē vienu otru

--- Assets/Scripts/ScrollScript.cs
+++ Assets/Scripts/ScrollScript.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScrollScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Image characterImage;
+    public Sprite[] characterSprites;
+    public Slider heightSlider;
+    public Slider widthSlider;
+    public void ChangeCharacterImage(int index)
     {
-        
+        characterImage.GetComponent<Image>().sprite = characterSprites[index];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeWidth()
     {
-        
+        float widthValue = widthSlider.GetComponent<Slider>().value;
+        characterImage.transform.localScale = new Vector2(1.1f * widthValue, 1);
     }
+
+    public void ChangeHeight()
+    {
+        float sizeValue = heightSlider.GetComponent<Slider>().value;
+        characterImage.transform.localScale = new Vector2(1, 1.1f * sizeValue);
+    }
+
 }
