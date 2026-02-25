ed139667267ea76ad386efd0b4551e6142155a34
VilheIms <boysepticeye@gmail.com>
2026-02-25 22:39:58 +0200

    Mainot character, mainās tā description. Tagad mēģināšu izveidot bikses utml.

--- Assets/Scripts/ScrollScript.cs
+++ Assets/Scripts/ScrollScript.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,10 +13,21 @@ public class ScrollScript : MonoBehaviour
     public Slider heightSlider;
     public Slider widthSlider;
     public int characterIndex;
+    public TMP_Text textField;
     public void ChangeCharacterImage(int index)
     {
         characterIndex = index;
         characterImage.GetComponent<Image>().sprite = characterSprites[index];
+        if(index == 0)
+        {
+            textField.GetComponent<TMP_Text>().text = "Character: \n A standard citizen of robloxia, ready to get dressed for work.";
+        }else if(index == 1)
+        {
+            textField.GetComponent<TMP_Text>().text = "Character: \n An adventurous man, getting ready for the next adventure.";
+        }else if(index == 2)
+        {
+            textField.GetComponent<TMP_Text>().text = "Character: \n A rare noob, who just wants to pwn his own kind.";
+        }
     }
     public void ToggleShirts(bool value)
     {
